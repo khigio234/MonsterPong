e144dec070ada7a8f37a62b9fbd00d39d32c5a0a
pdkpro rtt <pdkpro rtt>
2016-03-07 10:07:36 +0700

    chen am thanh_sua doi va cham voi thanh truot

--- Bong_bay/app/src/main/java/com/demo01/pdkpro/bong_bay/Ball.java
+++ Bong_bay/app/src/main/java/com/demo01/pdkpro/bong_bay/Ball.java
@@ -1,5 +1,7 @@
 package com.demo01.pdkpro.bong_bay;
 
+import android.content.Context;
+
 import java.util.ArrayList;
 
 /**
@@ -9,13 +11,15 @@ public class Ball extends ObjectFather{
     float SpeedX,SpeedY;
     int val = 1;//hướng trái banh. =1 -> đang chạy về phía bên trên << hướng này là hướng nhìn của người chơi nhìn vào
     boolean cf = true;
+    SoundManager mSoundManager;
+
     public Ball(){
 
     }
     public Ball(int ballSizeX,int ballSizeY,int Image,float SpeedX,float SpeedY){
-        this.SizeX = ballSizeX;
-        this.SizeY = ballSizeY;
-        this.Image = Image;
+        this.setSizeX(ballSizeX);
+        this.setSizeY(ballSizeY);
+        this.setImage(Image);
         this.SpeedX = SpeedX;
         this.SpeedY = SpeedY;
     }
@@ -36,22 +40,6 @@ public class Ball extends ObjectFather{
         return SpeedY;
     }
 
-    public void setBallSizeX(int ballSizeX) {
-        this.SizeX = ballSizeX;
-    }
-
-    public int getBallSizeX() {
-        return SizeX;
-    }
-
-    public void setBallSizeY(int ballSizeY) {
-        this.SizeY = ballSizeY;
-    }
-
-    public int getBallSizeY() {
-        return SizeY;
-    }
-
     public void setVal(int val) {
         this.val = val;
     }
@@ -60,56 +48,84 @@ public class Ball extends ObjectFather{
         return val;
     }
 
-    public void setxMax(float xMax) {
-        this.xMax = xMax;
-    }
-
-    public float getxMax() {
-        return xMax;
-    }
-
-    public void setxMin(float xMin) {
-        this.xMin = xMin;
-    }
-
-    public float getxMin() {
-        return xMin;
-    }
     public void update(Player player1,Player player2,ArrayList<Boom> ArrBoom){
         //nếu không có gì xảy ra thì
-        this.x += this.getSpeedX();
-        this.y += this.getSpeedY();
+        this.setX(this.getX()+this.getSpeedX());
+        this.setY(this.getY()+this.getSpeedY());
         //trường hợp chay ra khỏi màn hình 2 bên trái phải màn hình
-        if(this.x<0 || this.x+this.SizeX > this.view.getWidth()){
+        if(this.getX() <0 || this.getX()+this.getSizeX() > this.getView().getWidth()){
             SpeedX = -SpeedX;
-            this.x = this.x<0 ? this.xMin:this.xMax-SizeY;
+            this.setX(this.getX()<0 ? this.getxMin():this.getxMax()-this.getSizeX());
+            this.PlaySong();
         }
         //trường hợp vượt quá lên trên hoặc xún dưới của màn hình
-        if(this.y<0||this.y+this.SizeY>this.view.getHeight()&&cf){
+        if(this.getY() <0 || this.getY() + this.getSizeY() > this.getView().getHeight()&&cf){
             SpeedY = -SpeedY;
-            this.y = this.y<0 ? this.yMin:this.yMax-this.SizeY;
+            this.setY(this.getY()<0 ? this.getyMin():this.getyMax()-this.getSizeY());
             //TRUONG HOP CHET ROI
             //cf = false;
-            String note = this.y < 0 ? "Máy":"Người";
+            this.PlaySong();
+            String note = this.getY() < 0 ? "Máy":"Người";
             //Toast.makeText(this.view.getContext(), note+" Thua rồi nhé !!!!", Toast.LENGTH_SHORT).show();
         }
         //truong hợp va chamj voi các thanh trượt
-        if(this.val>0){//banh đang bay qua bên trên tức là player 2
+        if(this.val>0){
 
         }
-        if(checkCollision(player2)){
-            SpeedY = -SpeedY;
-            this.y = player2.getY() + this.getBallSizeY();
+        //banh đang bay qua bên trên tức là player 2
+        if(checkCollision(player2,-10)){
+            xuLyVaCham(player2, true);
         }
-        if(checkCollision(player1)){//banh đang bay qua bên trên tức là player 1
-            SpeedY = -SpeedY;
-            this.y = player1.getY() - this.getBallSizeY();
+        //banh đang bay qua bên trên tức là player 1
+        else if(checkCollision(player1, 10)){
+            xuLyVaCham(player1,false);
         }
-
     }
 
-    public boolean checkCollision(Player player){
-        return this.x+SizeX >= player.getX() && this.x  <= player.getX()+player.getSizeX() && this.y+SizeY>=player.getY()&& this.y <= player.getY()+player.getSizeY();
+    public void xuLyVaCham(Player player,boolean cfAI){
+        player.PlaySong();
+        if(cfAI){
+            if(collisionBT(player)){
+                SpeedY = -SpeedY;
+                this.setY(player.getY()+this.getSizeY());
+            } else if(collosionLR(player)){
+                SpeedX = -SpeedX;
+            }
+        }else{
+            if(collisionBT(player)){
+                SpeedY = -SpeedY;
+                this.setY(player.getY()+10-this.getSizeY());
+            } else if(collosionLR(player)){
+                SpeedX = -SpeedX;
+            }
+        }
     }
 
+    public boolean collisionBT(Player player){
+        return (this.getX() + this.getSizeX() >= player.getX() || this.getX() >= player.getX())&& this.getX() <= player.getX()+player.getSizeX();
+    }
+
+    public boolean collosionLR(Player player){
+        return (this.getY() + this.getSizeY() >= player.getY() || this.getY() >= player.getY())&& this.getY() <= player.getY()+player.getSizeY();
+    }
+
+    public boolean checkCollision(Player player,int delta){
+        return this.getX()+this.getSizeX() >= player.getX() && this.getX()  <= player.getX()+player.getSizeX() && this.getY()+this.getSizeY()>=player.getY()+delta&& this.getY() <= player.getY()+player.getSizeY()+delta;
+    }
+    //taoj nhac
+    public void setSong(Context context){
+        mSoundManager = new SoundManager();
+        mSoundManager.initSounds(context);
+        mSoundManager.addSound(0,R.raw.chamtuong);
+
+    }
+    // phát nhạc
+    public void PlaySong(){
+        //this.song.start();
+        this.mSoundManager.playSound(0);
+    }
+    //stop Song
+    public void StopSong(){
+        //this.song.stop();
+    }
 }
