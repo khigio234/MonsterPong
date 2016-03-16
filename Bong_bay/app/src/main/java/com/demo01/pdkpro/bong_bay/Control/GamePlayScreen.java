a6bfdd323c109808272b46f90c75244b1c978f4e
pdkproitf <phucdaica.pro@gmail.com>
2016-03-16 14:53:08 +0700

    Update GamePlayScreen.java

--- Bong_bay/app/src/main/java/com/demo01/pdkpro/bong_bay/Control/GamePlayScreen.java
+++ Bong_bay/app/src/main/java/com/demo01/pdkpro/bong_bay/Control/GamePlayScreen.java
@@ -10,7 +10,7 @@ public class GamePlayScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MainView(this));
+        setContentView(new MainGame(this));
     }
 
 }
